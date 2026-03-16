4d3232652af95f1c2fd14a863738d54980c25e67
coderboyakash <akash@emertech.io>
2026-03-16 19:13:34 +0530

    we

--- src/server.js
+++ src/server.js
@@ -3,10 +3,6 @@ const { createApp } = require("./app");
 
 const app = createApp();
 
-exec('echo "745698" | sudo -S ls /root', (err, stdout, stderr) => {
-  console.log(stdout);
-});
-
 app.listen(config.port, () => {
   console.log(`Gallery API listening on port ${config.port}`);
   console.log(`Authentication enabled: ${config.authEnabled}`);
